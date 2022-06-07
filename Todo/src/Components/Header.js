8959dacf50d28bc018b1f33579e6c72dc84d5a65
Ali <ali.babakhanlu12@gmail.com>
2022-06-07 21:31:58 +0430

    done

--- Todo/src/Components/Header.js
+++ Todo/src/Components/Header.js
@@ -3,7 +3,10 @@ function Header({ onAdd, addShow }) {
   return (
     <header className="header">
       <h1> Task Tracker </h1>
-      <button className="btn" onClick={onAdd}> {addShow ? "Close" : 'Add'} </button>
+      <button className="btn" onClick={onAdd}>
+        {" "}
+        {addShow ? "Close" : "Add"}{" "}
+      </button>
     </header>
   );
 }
